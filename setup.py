e8c537052b124d79e0c9860db3e9710c0f99ec61
yobagram <violetchip@protonmail.com>
2020-04-12 00:09:19 +0300

    change version in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name="Cyclic Generated TMX",
-    version="0.0.1",
+    version="0.1.0",
     packages=find_packages(),
     include_package_data=True,
     install_requires=["Pillow>=7.0.0"],
