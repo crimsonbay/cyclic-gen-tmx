f07f88d17441be8b4ac3ca09ccc38081f1d07485
yobagram <violetchip@protonmail.com>
2020-04-18 15:06:57 +0300

    add long_description & classifiers in setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ setup(
     name="Cyclic Generated TMX",
     license='MIT',
     description='Read, change, write, generate, create animated images TMX maps.',
+    long_description='Read, change, write, generate, create animated images TMX maps.',
     author='yobagram',
     url='https://github.com/yobagram/cyclic-gen-tmx',
     download_url='https://github.com/yobagram/cyclic-gen-tmx/archive/v_01.tar.gz',
@@ -11,4 +12,12 @@ setup(
     include_package_data=True,
     install_requires=["Pillow>=7.0.0"],
     keywords=['tmx', 'map', 'generation', 'save', 'image'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
 )
